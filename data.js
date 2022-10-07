ffda9ad618f8589aa25c921bc19d9fd614c8f28e
shanew-NAIT <93292360+shanew-NAIT@users.noreply.github.com>
2022-10-07 13:35:41 -0600

    test

---
+++ data.js
@@ -0,0 +1,388 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-largebooth",
+      "name": "largebooth",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-makerspace-6",
+      "name": "makerspace 6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.4558239894997893,
+        "pitch": 0.043699106678461774,
+        "fov": 1.5244891874381097
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6367206257297946,
+          "pitch": 0.14821442193143497,
+          "rotation": 0,
+          "target": "5-makerspace2"
+        },
+        {
+          "yaw": -0.2101222674517551,
+          "pitch": 0.1333159321234163,
+          "rotation": 0,
+          "target": "4-makerspace3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4002144978927937,
+          "pitch": 0.16964465305009213,
+          "title": "Ultimaker 3D Printer",
+          "text": "Make cool 3D stuff!"
+        },
+        {
+          "yaw": 2.0615838945374554,
+          "pitch": 0.06332851028321329,
+          "title": "Tile wall",
+          "text": "CNC tiles<div><br></div>"
+        },
+        {
+          "yaw": 2.649534976532319,
+          "pitch": 0.26906150177857135,
+          "title": "3D Scanning station",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.311343708214828,
+          "pitch": 0.1651029523652472,
+          "title": "Just in case...",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.1321059642989777,
+          "pitch": 0.15670371844194442,
+          "title": "Display cabinet",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.7975018564346374,
+          "pitch": 0.0446978997396581,
+          "title": "Makerbot 3D Printer<div><br></div>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.8499821480542042,
+          "pitch": 0.48291539827360097,
+          "title": "Ultimaker 2Go",
+          "text": "You can take this one home with you!"
+        }
+      ]
+    },
+    {
+      "id": "2-makerspace5",
+      "name": "makerspace5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.135555194884189,
+          "pitch": 0.18058377477541754,
+          "rotation": 0,
+          "target": "5-makerspace2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.187099312277817,
+          "pitch": 0.3275874227581035,
+          "title": "Laser Engraver",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.4813946844280235,
+          "pitch": 0.3818152298000346,
+          "title": "Light table",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.12625332727796845,
+          "pitch": 0.10231127943505314,
+          "title": "Vinyl Cutter",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-makerspace4",
+      "name": "makerspace4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09224964680841907,
+          "pitch": 0.06849512070176544,
+          "rotation": 6.283185307179586,
+          "target": "2-makerspace5"
+        },
+        {
+          "yaw": -3.0922067555414277,
+          "pitch": 0.1372039584975795,
+          "rotation": 0,
+          "target": "1-makerspace-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-makerspace3",
+      "name": "makerspace3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05693688416847564,
+          "pitch": 0.21143760478388174,
+          "rotation": 0,
+          "target": "1-makerspace-6"
+        },
+        {
+          "yaw": 0.878560990431847,
+          "pitch": 0.28166526430476146,
+          "rotation": 0,
+          "target": "6-makerspace1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-makerspace2",
+      "name": "makerspace2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08996732366384919,
+          "pitch": 0.15156708523244333,
+          "rotation": 12.566370614359176,
+          "target": "2-makerspace5"
+        },
+        {
+          "yaw": -3.108840388147401,
+          "pitch": 0.1368796753493946,
+          "rotation": 0,
+          "target": "1-makerspace-6"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.446350047079294,
+          "pitch": 0.300218390433427,
+          "title": "Carvey",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.37808222745995224,
+          "pitch": 0.16625465322813326,
+          "title": "X-Carve",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.7775730521097692,
+          "pitch": 0.09550869115989968,
+          "title": "Drill Press",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-makerspace1",
+      "name": "makerspace1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.9273306169231237,
+        "pitch": 0.15582504487598392,
+        "fov": 1.2210020199313556
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6131769903204081,
+          "pitch": 0.21329134242276915,
+          "rotation": 0,
+          "target": "4-makerspace3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.459359996299181,
+          "pitch": 0.0007287722722679746,
+          "title": "We are also a library!",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.2275947062502297,
+          "pitch": 0.4274375929209775,
+          "title": "Resin 3D Printer",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "NAIT Makerspace",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
